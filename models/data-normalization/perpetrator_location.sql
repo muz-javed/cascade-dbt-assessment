8c1650d3822137eaeb1af77db21d364d09a431bc
Muzammil Javed <muzammil.jvd@outlook.com>
2024-08-12 19:37:40 +0000

    .

--- models/data-normalization/perpetrator_location.sql
+++ models/data-normalization/perpetrator_location.sql
@@ -1,12 +1,9 @@
 select
-    row_number() over (order by city, latitude, longitude) as id, city_agent_id, city, latitude, longitude
+    row_number() over (order by city, latitude, longitude) as id, city, latitude, longitude
 from
     (
-        select city, latitude, longitude, comb_df.country, comb_df.city_agent, city_agent_df.id as city_agent_id
-        from {{ ref("combined_data_1NF") }} as comb_df
+        select city, latitude, longitude, country
+        from {{ ref("combined_data_1NF") }}
 
-        left join {{ ref('city_agent') }} as city_agent_df
-        on comb_df.city_agent = city_agent_df.city_agent and comb_df.country = city_agent_df.country
-
-        group by 1, 2, 3, 4, 5, 6
+        group by 1, 2, 3, 4
     )
